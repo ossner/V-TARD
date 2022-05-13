780b1690befceed5074ab4457d0c4b5ab0a6cea4
Sebastian Oßner <github@sebastianoner.com>
2022-05-13 12:10:23 +0200

    remove port argument

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from camera import Camera
 app = Flask(__name__)
 
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -24,4 +23,4 @@ def video_feed():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5001, debug=True)
+    app.run(host='0.0.0.0', debug=True)
